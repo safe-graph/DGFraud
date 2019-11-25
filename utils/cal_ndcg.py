1823b1cdf4b4c9345138d1b04c6b475cbefd4d54
Yingtong Dou <ytongdou@gmail.com>
2019-11-25 13:54:56 -0600

    upload the utils folder

---
+++ utils/cal_ndcg.py
@@ -0,0 +1,85 @@
+"""Metrics to compute the model performance."""
+
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import make_scorer
+
+def dcg_score(y_true, y_score, k=10, gains="exponential"):
+    """Discounted cumulative gain (DCG) at rank k
+    Parameters
+    ----------
+    y_true : array-like, shape = [n_samples]
+        Ground truth (true relevance labels).
+    y_score : array-like, shape = [n_samples]
+        Predicted scores.
+    k : int
+        Rank.
+    gains : str
+        Whether gains should be "exponential" (default) or "linear".
+    Returns
+    -------
+    DCG @k : float
+    """
+    order = np.argsort(y_score)[::-1]
+    y_true = np.take(y_true, order[:k])
+
+    if gains == "exponential":
+        gains = 2 ** y_true - 1
+    elif gains == "linear":
+        gains = y_true
+    else:
+        raise ValueError("Invalid gains option.")
+
+    # highest rank is 1 so +2 instead of +1
+    discounts = np.log2(np.arange(len(y_true)) + 2)
+    return np.sum(gains / discounts)
+
+
+def ndcg_score(y_true, y_score, k=10, gains="exponential"):
+    """Normalized discounted cumulative gain (NDCG) at rank k
+    Parameters
+    ----------
+    y_true : array-like, shape = [n_samples]
+        Ground truth (true relevance labels).
+    y_score : array-like, shape = [n_samples]
+        Predicted scores.
+    k : int
+        Rank.
+    gains : str
+        Whether gains should be "exponential" (default) or "linear".
+    Returns
+    -------
+    NDCG @k : float
+    """
+    best = dcg_score(y_true, y_true, k, gains)
+    actual = dcg_score(y_true, y_score, k, gains)
+    return actual / best
+
+def ranking_precision_score(y_true, y_score, k=10):
+    """Precision at rank k
+    Parameters
+    ----------
+    y_true : array-like, shape = [n_samples]
+        Ground truth (true relevance labels).
+    y_score : array-like, shape = [n_samples]
+        Predicted scores.
+    k : int
+        Rank.
+    Returns
+    -------
+    precision @k : float
+    """
+    unique_y = np.unique(y_true)
+
+    if len(unique_y) > 2:
+        raise ValueError("Only supported for two relevance levels.")
+
+    pos_label = unique_y[1]
+    n_pos = np.sum(y_true == pos_label)
+
+    order = np.argsort(y_score)[::-1]
+    y_true = np.take(y_true, order[:k])
+    n_relevant = np.sum(y_true == pos_label)
+
+    # Divide by min(n_pos, k) such that the best achievable score is always 1.0.
+    return float(n_relevant) / min(n_pos, k)
