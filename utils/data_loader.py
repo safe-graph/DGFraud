0ca60e4d59ad96d0950841929591e7fef32dc6b4
dyt <572566926@qq.com>
2019-12-18 21:51:53 +0800

    add utils

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,18 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 import scipy.io as sio
-import scipy.sparse as sp
 
-
-# symmetrically normalize adjacency matrix
-def normalize_adj(adj):
-    adj = adj + sp.eye(adj.shape[0])
-    adj = sp.coo_matrix(adj)
-    rowsum = np.array(adj.sum(1))
-    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
-    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
-    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).A
 
 
 def read_data_dzdp():
