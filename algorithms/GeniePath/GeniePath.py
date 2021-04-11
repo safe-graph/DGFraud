a9c4f8f6cca09848803c0e04e308fbc1650c1016
visitworld123 <44522582+visitworld123@users.noreply.github.com>
2021-04-12 01:02:03 +0800

    Update GeniePath to tf2

--- algorithms/GeniePath/GeniePath.py
+++ algorithms/GeniePath/GeniePath.py
@@ -20,98 +20,68 @@ import tensorflow as tf
 from base_models.layers import GeniePathLayer
 from algorithms.base_algorithm import Algorithm
 from utils import utils
+from utils.metrics import *
 
 
 class GeniePath(Algorithm):
 
-    def __init__(self,
-                 session,
-                 nodes,
-                 in_dim,
-                 out_dim,
-                 dim,
-                 lstm_hidden,
-                 heads,
-                 layer_num,
-                 class_size):
-        self.nodes = nodes
-        self.in_dim = in_dim
-        self.out_dim = out_dim
-        self.dim = dim
-        self.lstm_hidden = lstm_hidden
-        self.heads = heads
-        self.layer_num = layer_num
-        self.class_size = class_size
+    def __init__(self, input_dim, output_dim, args):
+        super().__init__()
 
-        self.placeholders = {'a': tf.placeholder(tf.float32, [1, self.nodes, self.nodes], 'adj'),
-                             'x': tf.placeholder(tf.float32, [self.nodes, self.in_dim], 'nxf'),
-                             'batch_index': tf.placeholder(tf.int32, [None], 'index'),
-                             't': tf.placeholder(tf.float32, [None, self.out_dim], 'labels'),
-                             'lr': tf.placeholder(tf.float32, [], 'learning_rate'),
-                             'mom': tf.placeholder(tf.float32, [], 'momentum'),
-                             'num_features_nonzero': tf.placeholder(tf.int32)}
+        self.nodes_num = args.nodes_num
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.GAT_output_dim = args.GAT_output_dim
+        self.lstm_hidden = args.lstm_hidden
+        self.GAT_heads = args.GAT_heads
+        self.layer_num = args.layer_num
+        self.class_size = args.class_size
+        # self.trainable_variables = []
 
-        loss, probabilities = self.forward_propagation()
-        self.loss, self.probabilities = loss, probabilities
-        self.l2 = tf.contrib.layers.apply_regularization(tf.contrib.layers.l2_regularizer(0.01),
-                                                         tf.trainable_variables())
+        # Fully connection initialization
+        self.Fully_connected_net_input = tf.keras.layers.Dense(self.GAT_output_dim, activation=lambda x: x)
+        self.Fully_connected_net_output = tf.keras.layers.Dense(self.output_dim, activation=lambda x: x)
 
-        self.pred = tf.one_hot(tf.argmax(self.probabilities, 1), self.out_dim)
-        print(self.pred.shape)
-        self.correct_prediction = tf.equal(tf.argmax(self.probabilities, 1), tf.argmax(self.placeholders['t'], 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, "float"))
-        print('Forward propagation finished.')
+        # GeniePath layers initialization
+        self.layers_ = []
+        for _ in range(self.layer_num):
+            self.layers_.append(GeniePathLayer(self.nodes_num, self.input_dim, self.GAT_output_dim, self.lstm_hidden, self.GAT_heads))
 
-        self.sess = session
-        self.optimizer = tf.train.AdamOptimizer(self.placeholders['lr'])
-        gradients = self.optimizer.compute_gradients(self.loss + self.l2)
-        capped_gradients = [(tf.clip_by_value(grad, -5., 5.), var) for grad, var in gradients if grad is not None]
-        self.train_op = self.optimizer.apply_gradients(capped_gradients)
-        self.init = tf.global_variables_initializer()
-        print('Backward propagation finished.')
 
-    def forward_propagation(self):
-        with tf.variable_scope('genie_path_forward'):
-            x = self.placeholders['x']
-            x = x[None, :]
-            x = tf.contrib.layers.fully_connected(x, self.dim, activation_fn=lambda x: x)
+    def __call__(self, inputs):
+        """
+        @param inputs: include support, x, label, mask
+        support: a list of the sparse adjacency Tensor
+        x: the node feature
+        label: the label tensor
+        mask: a list of mask tensors to obtain the data index
+        @return:
+        """
 
-            gplayers = [GeniePathLayer(self.placeholders, self.nodes, self.in_dim, self.dim)
-                        for i in range(self.layer_num)]
-            for i, l in enumerate(gplayers):
-                x, (h, c) = gplayers[i].forward(x, self.placeholders['a'], self.lstm_hidden, self.lstm_hidden)
-                x = x[None, :]
-            self.check = x
-            x = tf.contrib.layers.fully_connected(x, self.out_dim, activation_fn=lambda x: x)
-            x = tf.squeeze(x, 0)
-            print('geniePath embedding over!')
+        supports, x, label, idx_mask = inputs
 
-        with tf.variable_scope('classification'):
-            batch_data = tf.matmul(tf.one_hot(self.placeholders['batch_index'], self.nodes), x)
-            # W = tf.get_variable(name='weights',
-            #                     shape=[self.out_dim, self.class_size],
-            #                     initializer=tf.contrib.layers.xavier_initializer())
-            # b = tf.get_variable(name='bias', shape=[1, self.class_size], initializer=tf.zeros_initializer())
-            # logits = tf.matmul(batch_data, W) + b
-            logits = batch_data
-            loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=self.placeholders['t'], logits=logits)
+        # forward propagation
+        x = tf.expand_dims(x, axis=0)
+        x = self.Fully_connected_net_input(x)
+        # self.trainable_variables.append(self.Fully_connected_net_input.trainable_variables)
+        for layer in self.layers_:
+            x = layer((x, supports))
+            # self.trainable_variables.append(layer.trainable_variables)
+            x = tf.expand_dims(x, axis=0)
+        x = self.Fully_connected_net_output(x)
+        # self.trainable_variables.append(self.Fully_connected_net_output.trainable_variables)
+        # self.trainable_variables = sum(self.trainable_variables, [])
+        GeniePath_out = tf.squeeze(x,0)
 
-        return loss, tf.nn.softmax(logits)
+        # get masked data
+        masked_data = tf.gather(GeniePath_out, idx_mask)
+        masked_label = tf.gather(label, idx_mask)
 
-    def train(self, x, a, t, b, learning_rate=1e-2, momentum=0.9):
-        feed_dict = utils.construct_feed_dict(x, a, t, b, learning_rate, momentum, self.placeholders)
-        outs = self.sess.run(
-            [self.train_op, self.loss, self.accuracy, self.pred, self.probabilities],
-            feed_dict=feed_dict)
-        loss = outs[1]
-        acc = outs[2]
-        pred = outs[3]
-        prob = outs[4]
-        return loss, acc, pred, prob
+        # loss and accuracy
+        logits = masked_data
+        loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(masked_label, logits))
+        acc = accuracy(logits, masked_label)
 
-    def test(self, x, a, t, b, learning_rate=1e-2, momentum=0.9):
-        feed_dict = utils.construct_feed_dict(x, a, t, b, learning_rate, momentum, self.placeholders)
-        acc, pred, probabilities, tags = self.sess.run(
-            [self.accuracy, self.pred, self.probabilities, self.correct_prediction],
-            feed_dict=feed_dict)
-        return acc, pred, probabilities, tags
+        return loss, acc
+
+
